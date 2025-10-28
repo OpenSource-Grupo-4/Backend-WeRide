f4ca11b95505c08cfc959b810adb1e0a9caf7bd7
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-10-27 23:06:09 -0500

    initial commit

---
+++ src/main/java/org/example/backendweride/BackendWerideApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendweride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWerideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWerideApplication.class, args);
+    }
+
+}
