07463094eca999f75e40f98df67b0606b5ccd11b
jhimyromeromeza <romeromezajhimy0@gmail.com>
2025-12-06 09:29:14 -0500

    fix: delete mkdir bounded context Profile

--- src/main/java/org/example/backendweride/platform/profile/domain/model/queries/GetProfileByIdQuery.java
+++
@@ -1,4 +0,0 @@
-package org.example.backendweride.platform.profile.domain.model.queries;
-
-public record GetProfileByIdQuery(Long profileId) {
-}
--- src/main/java/org/example/backendweride/platform/profile/domain/model/queries/GetProfileByUserIdQuery.java
+++
@@ -1,4 +0,0 @@
-package org.example.backendweride.platform.profile.domain.model.queries;
-
-public record GetProfileByUserIdQuery(Long userId) {
-}
